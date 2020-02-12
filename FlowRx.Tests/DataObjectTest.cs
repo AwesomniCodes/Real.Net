ededac18766eb64dbf9d2c5e2247982b72ee47e1
Felix Keil <keil.felix@outlook.com>
2020-02-12 23:32:26 +0100

    DummyTest for pipeline and updated project informations

---
+++ FlowRx.Tests/DataObjectTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowRx.Tests
+{
+    [TestClass]
+    public class DataObjectTest
+    {
+        [TestMethod]
+        public void DummyTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
