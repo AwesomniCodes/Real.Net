aebdab45978aacbae465bf146a1d34fac2f959f6
Felix Keil <keil.felix@outlook.com>
2020-02-19 17:14:03 +0100

    Pseudo code from old console application

--- FlowRx.Tests/DataObjectTest.cs
+++ FlowRx.Tests/DataObjectTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Awesomni.Codes.FlowRx.DataSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,7 +12,37 @@ namespace Awesomni.Codes.FlowRx.Tests
         [TestMethod]
         public void DummyTest()
         {
+
+            var system1 = new DataDirectory("TestRoot");
+            var system2 = new DataDirectory("TestRoot2");
+            //system1.Link.AsLogOutput().Subscribe(logMessage => _log.Info($"system1:{logMessage}"));
+            //system2.Link.AsLogOutput().Subscribe(logMessage => _log.Info($"system2:{logMessage}"));
+            
+            //var storeSync = new DataSync();
+
+            //storeSync.SyncObjects.Add(system1.Root);
+            //storeSync.SyncObjects.Add(system2.Root);
+
+
+
+            var subFolder = system1.GetOrCreateDirectory("TestDirectory");
+
+            var testString = subFolder.GetOrCreate("TestString", "TestString");
+            var testInt = subFolder.GetOrCreate("TestInt", 23);
+            var testDouble = subFolder.GetOrCreate("TestDouble", 23.0);
+            var testBool = subFolder.GetOrCreate("TestBool", true);
+
+
+            var subFolder2 = system2.GetOrCreateDirectory("TestDirectory");
+            var testBool2 = subFolder2.GetOrCreate("TestBool", true);
+            testInt.OnNext(20);
+            testDouble.OnNext(1.5);
+            testInt.OnNext(24);
+            testString.OnNext("NewTestString");
+            testBool.OnNext(false);
+            testBool2.OnNext(true);
             Assert.IsTrue(true);
+
         }
     }
 }
